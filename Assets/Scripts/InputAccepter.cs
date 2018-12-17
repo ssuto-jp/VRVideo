7d91dc7bc89b22386275ef2780af63fa89a52029
suto <ss.time2531@gmail.com>
2018-12-18 00:06:01 +0900

    コントローラーの入力

---
+++ Assets/Scripts/InputAccepter.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class InputAccepter : MonoBehaviour
+{
+    public GameObject sphere;
+
+    private void OnEnable()
+    {
+        VRPlayerInput.OnTriggerUp += TriggerUp;
+        VRPlayerInput.OnTriggerDown += TriggerDown;
+        VRPlayerInput.OnTouchpadUp += TouchpadUp;
+        VRPlayerInput.OnTouchpadDown += TouchpadDown;
+    }
+
+    private void OnDisable()
+    {
+        VRPlayerInput.OnTriggerUp -= TriggerUp;
+        VRPlayerInput.OnTriggerDown -= TriggerDown;
+        VRPlayerInput.OnTouchpadUp -= TouchpadUp;
+        VRPlayerInput.OnTouchpadDown -= TouchpadDown;
+    }
+
+    private void TriggerUp()
+    {
+        sphere.GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+
+    private void TriggerDown()
+    {
+        sphere.GetComponent<MeshRenderer>().material.color = Color.blue;
+    }
+
+    private void TouchpadUp()
+    {
+        sphere.GetComponent<MeshRenderer>().material.color = Color.yellow;
+    }
+
+    private void TouchpadDown()
+    {
+        sphere.GetComponent<MeshRenderer>().material.color = Color.green;
+    }
+}
---
+++ Assets/Scripts/VRPlayerInput.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class VRPlayerInput : MonoBehaviour
+{
+    public static UnityAction OnTriggerUp;
+    public static UnityAction OnTriggerDown;
+    public static UnityAction OnTouchpadUp;
+    public static UnityAction OnTouchpadDown;
+
+    private void Update()
+    {
+        CheckInput();
+    }
+
+    private void CheckInput()
+    {
+        if (OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            if (OnTriggerUp != null)
+                OnTriggerUp();
+        }
+
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            if (OnTriggerDown != null)
+                OnTriggerDown();
+        }
+
+        if (OVRInput.GetUp(OVRInput.Button.PrimaryTouchpad))
+        {
+            if (OnTouchpadUp != null)
+                OnTouchpadUp();
+        }
+
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryTouchpad))
+        {
+            if (OnTouchpadDown != null)
+                OnTouchpadDown();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        OnTriggerUp = null;
+        OnTriggerDown = null;
+        OnTouchpadUp = null;
+        OnTouchpadDown = null;
+    }
+}
