8a74019b63ab5fba86b7443c99a69761e0e18a07
suto <ss.time2531@gmail.com>
2018-12-27 18:45:13 +0900

    再生・停止ボタン、シークバーを追加

--- Assets/Scripts/VideoController.cs
+++ Assets/Scripts/VideoController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 namespace RenderHeads.Media.AVProVideo
@@ -6,9 +7,11 @@ namespace RenderHeads.Media.AVProVideo
     public class VideoController : MonoBehaviour
     {
         [SerializeField] private MediaPlayer mediaPlayer;
+        [SerializeField] private Slider videoSeekSlider;
 
         private string videoPath;
         private MediaPlayer.FileLocation videoLocation = MediaPlayer.FileLocation.RelativeToStreamingAssetsFolder;
+        private float setVideoSeekSliderValue;
 
         private void Start()
         {
@@ -16,10 +19,46 @@ namespace RenderHeads.Media.AVProVideo
             PlayVideo(videoPath);
         }
 
+        private void Update()
+        {
+            if (mediaPlayer.Info != null && mediaPlayer.Info.GetDurationMs() > 0f)
+            {
+                float time = mediaPlayer.Control.GetCurrentTimeMs();
+                float duration = mediaPlayer.Info.GetDurationMs();
+                float d = Mathf.Clamp(time / duration, 0.0f, 1.0f);
+
+                setVideoSeekSliderValue = d;
+                videoSeekSlider.value = d;
+            }
+        }
+
         private void PlayVideo(string path)
         {
             if (path != null)
                 mediaPlayer.OpenVideoFromFile(videoLocation, path, true);
+        }
+
+        public void OnPlayButton()
+        {
+            mediaPlayer.Control.Play();
+        }
+
+        public void OnPauseButton()
+        {
+            mediaPlayer.Control.Pause();
+        }
+
+        public void OnRewindButton()
+        {
+            mediaPlayer.Control.Rewind();
+        }
+
+        public void OnVideoSeekSlider()
+        {
+            if (videoSeekSlider && videoSeekSlider.value != setVideoSeekSliderValue)
+            {
+                mediaPlayer.Control.Seek(videoSeekSlider.value * mediaPlayer.Info.GetDurationMs());
+            }
         }
 
         public void OnFinishedPlaying()
